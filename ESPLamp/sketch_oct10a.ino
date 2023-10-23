224a27384665b02000048ca05e9f6f8a4977544b
env116 <107906220+ToshaTosha@users.noreply.github.com>
2023-10-23 23:35:26 +0800

    update main

--- ESPLamp/sketch_oct10a.ino
+++ ESPLamp/sketch_oct10a.ino
@@ -6,11 +6,10 @@
 void setup(){
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  //bool is_wifi_on = start_AP_mode();
   bool is_wifi_on = init_WIFI(false);
   if(is_wifi_on){
-     server_init();
-    init_MQTT();
+        server_init();
+        init_MQTT();
     }
     String topic = "esp8266_" + id() + "/command";
     String state_topic = "esp8266_" + id() + "/state";
