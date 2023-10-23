089573bb5b766e9c804f438624dc2f07f733ed34
злата <118795314+dzhoshua@users.noreply.github.com>
2023-10-23 23:10:53 +0800

    Add files via upload

---
+++ ESPLamp/Config.h
@@ -0,0 +1,9 @@
+String AP_NAME="ESP8226";
+String AP_PASSWORD="ESP8226123";
+
+char CLI_SSID [] = "я";
+char CLI_PASS [] = "jsu4nzwmdajvj8y";
+int WEB_SERVER_PORT= 80;
+
+char mqtt_broker[] = "broker.emqx.io";
+const int mqtt_port = 1883;
