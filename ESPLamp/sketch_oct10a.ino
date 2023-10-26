17305b257f6b1b033dec565d06e7e204034e36d2
злата <118795314+dzhoshua@users.noreply.github.com>
2023-10-26 19:50:12 +0800

    fix sketch_oct10a.ino

--- ESPLamp/sketch_oct10a.ino
+++ ESPLamp/sketch_oct10a.ino
@@ -6,21 +6,18 @@
 void setup(){
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  bool is_wifi_on = init_WIFI(false);
-  if(is_wifi_on){
+  bool is_ap_on = start_AP_mode();
+  //bool is_wifi_on = init_WIFI(false);
+  if(is_ap_on){
         server_init();
-        init_MQTT();
+        //init_MQTT();
     }
-    String topic = "esp8266_zlataveronika/command";
-    String state_topic = "esp8266_zlataveronika/state";
-    mqtt_client.subscribe(topic.c_str());
-    mqtt_client.publish(state_topic.c_str(), "hello");
-    Serial.println("See me at " + state_topic);
-
 }
 
 void loop(){
   server.handleClient();
-  mqtt_client.loop();
+  if (mqtt_client.connected()){
+    mqtt_client.loop();
+  }
   delay(20);
 }
